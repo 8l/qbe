209a80bb669ae1c8d25a195b4d22cc1a1cfa1ddf
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2015-04-05 15:52:18 -0400

    run multiple rounds for stable timing

--- tmain.c
+++ tmain.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
+enum { NRounds = 150 };
+
 extern long f(void);
 
 int main()
 {
-	clock_t t0;
-	long l;
+	clock_t t0, tmin;
+	long i, l;
 
-	t0 = clock();
-	l = f();
-	t0 = clock() - t0;
+	tmin = 10 * CLOCKS_PER_SEC;
+	for (i=0; i<NRounds; i++) {
+		t0 = clock();
+		l = f();
+		t0 = clock() - t0;
+		if (t0 < tmin)
+			tmin = t0;
+	}
 	printf("f() = %ld\n", l);
 	printf("  %.4f secs\n", (double)t0/CLOCKS_PER_SEC);
 	return 0;
