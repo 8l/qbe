8cfcd87e50121f20be175d341bfddd55e35ab7c2
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2015-10-13 13:15:12 -0400

    no, 9 is not prime

--- minic/test/prime.c
+++ minic/test/prime.c
@@ -8,13 +8,13 @@ main() {
 
 	c = 0;
 	n = 2;
-	while (n < 1000) {
+	while (n < 5000) {
 		t = 2;
 		p = 1;
 		while (t*t <= n) {
 			if (n % t == 0)
 				p = 0;
-			t = t + 2;
+			t++;
 		}
 		if (p) {
 			if (c % 10 == 0)
