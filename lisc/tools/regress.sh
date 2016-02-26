a9a723e0089e39829dfc22d006c6c6b0873f1772
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-02-26 15:54:43 -0500

    also catch stderr

--- lisc/tools/regress.sh
+++ lisc/tools/regress.sh
@@ -4,8 +4,8 @@ for t in test/*
 do
 	printf "Test $t ... "
 
-	./lisc   $t 2>&1 > /tmp/out.0
-	./lisc.1 $t 2>&1 > /tmp/out.1
+	./lisc   $t >/tmp/out.0 2>&1
+	./lisc.1 $t >/tmp/out.1 2>&1
 
 	if diff /tmp/out.0 /tmp/out.1 > /dev/null
 	then
